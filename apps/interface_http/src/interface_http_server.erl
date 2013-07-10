f9edc7c18eaa5e13b6e6980dbcb5336914a6d634
Johnson Lau <gatement@gmail.com>
2013-07-10 23:44:26 +0800

    remove https

--- apps/interface_http/src/interface_http_server.erl
+++ apps/interface_http/src/interface_http_server.erl
@@ -9,7 +9,7 @@
 %% ===================================================================
 
 start() ->
-    {ok, erlang:spawn(?MODULE, run, [])}.
+	{ok, erlang:spawn(?MODULE, run, [])}.
 
 
 %% ===================================================================
@@ -17,27 +17,43 @@ start() ->
 %% ===================================================================
 
 run() ->
-    {ok, HttpPort} = application:get_env(http_port), 
-    {ok, HttpsPort} = application:get_env(https_port), 
-    {ok, KeyFile} = application:get_env(keyfile), 
-    {ok, CertFile} = application:get_env(certfile), 
+	{ok, Port} = application:get_env(port), 
+	{ok, KeyFile} = application:get_env(keyfile), 
+	{ok, CertFile} = application:get_env(certfile), 
 
-    PrivDir = code:priv_dir(interface_http),
-    DocRoot = PrivDir ++ "/site",
-    LogDir = PrivDir ++ "/../../../priv/log",
-    IncludeDir1 = PrivDir ++ "/../include",
-    IncludeDir2 = PrivDir ++ "/../../platform_core/include",
+	PrivDir = code:priv_dir(interface_http),
+	DocRoot = PrivDir ++ "/site",
+	LogDir = PrivDir ++ "/../../../priv/log",
+	IncludeDir1 = PrivDir ++ "/../include",
+	IncludeDir2 = PrivDir ++ "/../../platform_core/include",
 
-    filelib:ensure_dir(LogDir ++ "/"),
-    
-	GconfList = [ {logdir, LogDir},
+	filelib:ensure_dir(LogDir ++ "/"),
+
+	Id = "tools_platform",
+
+	GconfList = [
+		{id, Id},
+		{logdir, LogDir},
 		{include_dir, [IncludeDir1, IncludeDir2]},
 		{runmods, []}],
 
-	SconfList = [{listen, {0,0,0,0}},
+	Ssl = case KeyFile of
+		[] ->
+			undefined;
+		_ ->
+			#ssl{
+				keyfile = KeyFile,
+				certfile = CertFile
+			}
+	end,
+
+	SconfList = [
+		{port, Port},
+		{listen, {0,0,0,0}},
 		{docroot, DocRoot},
 		{arg_rewrite_mod, arg_rewriter},
 		{errormod_404, nofound},
+		{ssl, Ssl},
 		{appmods, [
 				{"/user/", rest_usr_user},
 				{"/setting/", rest_gbl_setting},
@@ -48,33 +64,11 @@ run() ->
 				{"/gallery/", rest_tool_gallery},
 				{"/device/", rest_tool_device},
 				{"/device_mgmt/", rest_device_mgmt}
-			]}],
-	
-	%% HTTP 
-    IdHttp = "tools_platform_http",
-	GconfListHttp = [{id, IdHttp} | GconfList],
-	SconfListHttp = [{port, HttpPort} | SconfList], 
+			]}
+	],
 
-    {ok, SCListHttp, GCHttp, ChildSpecsHttp} = yaws_api:embedded_start_conf(DocRoot, SconfListHttp, GconfListHttp, IdHttp),
-    [supervisor:start_child(interface_http_sup, Ch) || Ch <- ChildSpecsHttp],
-    yaws_api:setconf(GCHttp, SCListHttp),
-
-	%% HTTPS
-	case KeyFile of
-		[] ->
-			do_not_start;
-		_ ->
-			IdHttps = "tools_platform_https",
-			Ssl = #ssl{
-				keyfile = KeyFile,
-				certfile = CertFile
-			},
-			GconfListHttps = [{id, IdHttps} | GconfList],
-			SconfListHttps = lists:append([{port, HttpsPort}, {ssl, Ssl}], SconfList), 
-
-			{ok, SCListHttps, GCHttps, ChildSpecsHttps} = yaws_api:embedded_start_conf(DocRoot, SconfListHttps, GconfListHttps, IdHttps),
-			[supervisor:start_child(interface_http_sup, Ch) || Ch <- ChildSpecsHttps],
-			yaws_api:setconf(GCHttps, SCListHttps)
-	end,
+	{ok, SCList, GC, ChildSpecs} = yaws_api:embedded_start_conf(DocRoot, SconfList, GconfList, Id),
+	[supervisor:start_child(interface_http_sup, Ch) || Ch <- ChildSpecs],
+	yaws_api:setconf(GC, SCList),
 
 	ok.
