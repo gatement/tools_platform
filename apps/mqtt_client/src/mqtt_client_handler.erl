6d3b2eb670c31e81da3063c7e236117e40018554
JohnsonLau <gatement@gmail.com>
2013-08-18 21:51:43 +0800

    fix typo

--- apps/mqtt_client/src/mqtt_client_handler.erl
+++ apps/mqtt_client/src/mqtt_client_handler.erl
@@ -124,7 +124,7 @@ process_data_human_status(_ClientId, HumanStatus) ->
     %error_logger:info_msg("[~p] received [human status] data: ~p - ~p~n", [?MODULE, _ClientId, HumanStatus]),
 	case HumanStatus of
 		1 -> 
-			Msg = lists:flatten(io_lib:format("Activity was deteced [~s]", [tools:datetime_string('hh:mm:ss')])),
+			Msg = lists:flatten(io_lib:format("Motion detected! [~s]", [tools:datetime_string('hh:mm:ss')])),
 			mqtt_broker:send_msg(Msg);
 			
 		_ -> do_nothing
